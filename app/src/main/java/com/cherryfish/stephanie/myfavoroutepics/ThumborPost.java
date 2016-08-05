a46bd11e0ebefa7d71fdcd8f1d69514ef8da856e
sverlingo <stephanie.hemmer@gmail.com>
2016-08-05 17:23:40 -0500

    adding licence for okhttp library

--- app/src/main/java/com/cherryfish/stephanie/myfavoroutepics/ThumborPost.java
+++ app/src/main/java/com/cherryfish/stephanie/myfavoroutepics/ThumborPost.java
@@ -35,6 +35,23 @@ import okhttp3.Response;
 
 /**
  * Created by Stephanie Verlingo on 7/21/2016.
+ *
+ * OKHTTP Library License
+ * http://square.github.io/okhttp/
+ *
+ * Copyright 2016 Square, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
 public class ThumborPost extends AsyncTask<File, Void, Boolean> {
     String hostUrl = "http://188.226.195.167:8889";
